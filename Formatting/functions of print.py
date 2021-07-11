7406e29370b889160dbe29a3d228d6b8bfcb9754
Savag33 <alfapro8@gmail.com>
2021-07-12 02:37:30 +0300

    Some functions of print

---
+++ Formatting/functions of print.py
@@ -0,0 +1,14 @@
+# sep="..." - разделитпель
+
+print(1, 2, 3, sep="=")
+print()
+# end='...' - перенос нижней строки(по умолчанию \n)
+
+print(1, 2, 3, end=' ')
+print(4, 5, 6)
+
+print()
+
+rubles = 10
+kop = 99
+print("I have some %s rubles %s kopeek" %(rubles,kop)) # %s type of formatting
