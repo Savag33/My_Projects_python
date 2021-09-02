f7898a634f2786b0cb24da07ce53dc7076b62d09
Artem blazhenko <alfapro8@gmail.com>
2021-09-02 21:39:43 +0300

    Add new functions of print

---
+++ Functions of print/Func of print.py
@@ -0,0 +1,12 @@
+print(1, 2, 3)
+print(1, 2, 3, sep=',') # выбор раздилителя
+print()
+
+print(1, 2, end=' cool ')  # end - перенос строки
+print(3, 4)
+
+print()
+''' like formatting '''
+rubles = 99
+kop = 22
+print('I have %s rubles %s kepeyek'%(rubles,kop))
